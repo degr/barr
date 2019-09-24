4e0e9832eff7380bc3a2ddaebce52b338794320b
asavik <oleg.savik1996@gmail.com>
2019-09-24 18:04:37 +0300

    refactor code

--- src/main/java/org/kurento/tutorial/groupcall/websocket/command/JoinRoomCommand.java
+++ src/main/java/org/kurento/tutorial/groupcall/websocket/command/JoinRoomCommand.java
@@ -1,29 +1,22 @@
 package org.kurento.tutorial.groupcall.websocket.command;
 
-import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
-import org.kurento.tutorial.groupcall.auth.AuthorizationHandler;
-import org.kurento.tutorial.groupcall.dto.MessageDto;
 import org.kurento.tutorial.groupcall.services.UserRegistry;
 import org.kurento.tutorial.groupcall.websocket.Room;
 import org.kurento.tutorial.groupcall.websocket.UserSession;
 import org.springframework.web.socket.WebSocketSession;
 
-@AllArgsConstructor
 abstract class JoinRoomCommand {
-    private final UserRegistry sessionRegistry;
-    private final AuthorizationHandler authorizationHandler;
+    private final UserRegistry userRegistry;
+
+    public JoinRoomCommand(UserRegistry userRegistry) {
+        this.userRegistry = userRegistry;
+    }
 
     @SneakyThrows
-    void execute(MessageDto message, WebSocketSession webSocketSession, Room room) {
-        String login = message.getName();
-        String password = message.getPassword();
-        String token = authorizationHandler.authorize(login, password);
-        if (room == null) {
-            return;
-        }
-        UserSession userSession = new UserSession(login, token, room.getRoomKey(), webSocketSession, room.getMediaPipeline());
+    void joinRoom(String login, String token, WebSocketSession socketSession, Room room) {
+        UserSession userSession = new UserSession(login, token, room.getRoomKey(), socketSession, room.getMediaPipeline());
         room.join(userSession);
-        sessionRegistry.register(userSession);
+        userRegistry.register(userSession);
     }
 }
